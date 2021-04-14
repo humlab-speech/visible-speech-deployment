c2d81fb48facece1b12a84506221d56881f2aae0
Johan von Boer <johan.von.boer@umu.se>
2021-04-14 12:16:17 +0200

    Adapted for git-agent

--- docker/session-manager/build-session-images.sh
+++ docker/session-manager/build-session-images.sh
@@ -1,5 +1,11 @@
+mkdir -p ./scripts/git-agent
+cp -Rvp ../../git-agent/dist/* ./scripts/git-agent/
+
 echo "Building RStudio session image"
 docker build -t hs-rstudio-session -f hs-rstudio-session/Dockerfile .
 
 echo "Building Jupyter session image"
 docker build -t hs-jupyter-session -f hs-jupyter-session/Dockerfile .
+
+echo "Building Operations session image"
+docker build -t hs-operations-session -f hs-operations-session/Dockerfile .
