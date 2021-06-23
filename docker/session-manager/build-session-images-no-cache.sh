3894bcdc0bc0dd6c316f5b345d0142abee8485a3
Johan von Boer <johan.von.boer@umu.se>
2021-06-23 10:39:39 +0200

    renamed some things hs => visp

--- docker/session-manager/build-session-images-no-cache.sh
+++ docker/session-manager/build-session-images-no-cache.sh
@@ -1,12 +1,12 @@
 cp -Rvp ../../container-agent/dist ./container-agent
 
 echo "Building Operations session image"
-docker build --no-cache -t hs-operations-session -f hs-operations-session/Dockerfile .
+docker build --no-cache -t visp-operations-session -f operations-session/Dockerfile .
 
 echo "Building RStudio session image"
-docker build --no-cache -t hs-rstudio-session -f hs-rstudio-session/Dockerfile .
+docker build --no-cache -t visp-rstudio-session -f rstudio-session/Dockerfile .
 
 echo "Building Jupyter session image"
-docker build --no-cache -t hs-jupyter-session -f hs-jupyter-session/Dockerfile .
+docker build --no-cache -t visp-jupyter-session -f jupyter-session/Dockerfile .
 
 rm -R ./container-agent
--- docker/session-manager/build-session-images.sh
+++ docker/session-manager/build-session-images.sh
@@ -2,12 +2,12 @@ cd ../../container-agent && npm run build && cd ../docker/session-manager
 cp -Rvp ../../container-agent/dist ./container-agent
 
 echo "Building Operations session image"
-docker build -t hs-operations-session -f hs-operations-session/Dockerfile .
+docker build -t visp-operations-session -f operations-session/Dockerfile .
 
 echo "Building RStudio session image"
-docker build -t hs-rstudio-session -f hs-rstudio-session/Dockerfile .
+docker build -t visp-rstudio-session -f rstudio-session/Dockerfile .
 
 echo "Building Jupyter session image"
-docker build -t hs-jupyter-session -f hs-jupyter-session/Dockerfile .
+docker build -t visp-jupyter-session -f jupyter-session/Dockerfile .
 
 rm -R ./container-agent
