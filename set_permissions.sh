9d247c791965137af51fbe6b15dc87bf166de9af
Johan von Boer <johan.von.boer@umu.se>
2021-11-08 12:13:47 +0100

    including apach uploads dir

--- set_permissions.sh
+++ set_permissions.sh
@@ -1,2 +1,3 @@
 #webapi dir should be set so that the www-data user (uid 33) within the edge-router container has write access
 chown -R 33 ./mounts/webapi
+chown -R 33 ./mounts/apache/apache/uploads
