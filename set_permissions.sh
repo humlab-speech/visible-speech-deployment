bacb84a4a0010318af6605f91c98b11c73f2a136
Johan von Boer <johan.von.boer@umu.se>
2022-03-11 14:44:25 +0100

    mongo perms

--- set_permissions.sh
+++ set_permissions.sh
@@ -1,3 +1,4 @@
 #webapi dir should be set so that the www-data user (uid 33) within the edge-router container has write access
 chown -R 33 ./mounts/webapi
 chown -R 33 ./mounts/apache/apache/uploads
+chown -R 999 ./mounts/mongo
