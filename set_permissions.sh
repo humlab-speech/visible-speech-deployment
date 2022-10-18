501a276a93e9f7df4bc1d303432ba97eb907be33
Johan von Boer <johan.von.boer@umu.se>
2022-10-18 13:24:52 +0200

    setting perms for session-manager mount dir

--- set_permissions.sh
+++ set_permissions.sh
@@ -1,5 +1,5 @@
-#webapi dir should be set so that the www-data user (uid 33) within the edge-router container has write access
 chown -R 33 ./mounts/webapi
 chown -R 33 ./mounts/apache/apache/uploads
 chown -R 999 ./mounts/mongo
 chown -R 33 ./certs/ssp-idp-cert
+chown -R 1000 mounts/session-manager
