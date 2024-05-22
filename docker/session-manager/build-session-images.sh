c7794144ccdace4b10dfad987039f7b8323ed82d
Johan von Boer <johan.von.boer@umu.se>
2024-05-22 10:51:39 +0200

    removed build of container-agent, removed vscode

--- docker/session-manager/build-session-images.sh
+++ docker/session-manager/build-session-images.sh
@@ -1,5 +1,3 @@
-cd ../../container-agent && npm run build && cd ../docker/session-manager && cp -Rvp ../../container-agent/dist ./container-agent
-
 #echo "Building Operations session image"
 docker build -t visp-operations-session -f operations-session/Dockerfile .
 
@@ -9,7 +7,3 @@ docker build -t visp-rstudio-session -f rstudio-session/Dockerfile .
 #echo "Building Jupyter session image"
 docker build -t visp-jupyter-session -f jupyter-session/Dockerfile .
 
-echo "Building VScode session image"
-docker build -t visp-vscode-session -f vscode-session/Dockerfile .
-
-rm -R ./container-agent
