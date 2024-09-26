741b630e88c03fa7d7f90d6ae3b914bd9e5594cc
Tomas Skotare <tomas.skotare@umu.se>
2024-09-26 13:49:38 +0200

    Updated the docker build files to use the matlab download script.
    Also changed nodejs from specific version to nodejs; Try to determine if this has a significant impact or not. The image could not be build otherwise, so it was a necessary change (or a workaround must be found)

--- docker/session-manager/build-session-images.sh
+++ docker/session-manager/build-session-images.sh
@@ -1,6 +1,11 @@
 #echo "Building Operations session image"
 docker build -t visp-operations-session -f operations-session/Dockerfile .
 
+if [ ! -f ./matlab_runtime_installer/current_matlab_install ]; then
+  echo "MATLAB Runtime not found, running download script..."
+  ./matlab-scripts/download_matlab_runtime_environment.sh
+fi
+
 #echo "Building RStudio session image"
 docker build -t visp-rstudio-session -f rstudio-session/Dockerfile .
 
