10fe2120ddab5664b755afddf2ab3954e9ff479f
Johan von Boer <johan.von.boer@umu.se>
2025-01-13 10:08:00 +0100

    removed copying of container-agent. it's better (in prod at least) to clone it in the Dockerfile

--- docker/session-manager/build-jupyter.sh
+++ docker/session-manager/build-jupyter.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 #echo "Building Jupyter session image"
-cp -Rp ../../container-agent/* ./files/container-agent/
 docker build -t visp-jupyter-session -f jupyter-session/Dockerfile .
 
